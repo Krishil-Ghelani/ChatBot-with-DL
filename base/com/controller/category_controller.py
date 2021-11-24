feb447347ad870c7edd586e645bb835c2c599f96
Krishil-Ghelani <krishil.work.all@gmail.com>
2021-11-24 10:00:32 +0530

    updated with sql

--- base/com/controller/category_controller.py
+++ base/com/controller/category_controller.py
@@ -1,8 +1,10 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 
 from chatbot.base import app
 from chatbot.base.com.dao.category_dao import TableDao
 from chatbot.base.com.vo.category_vo import TableVo
+
+import pymysql
 
 print('in controller')
 table_obj = TableVo()
@@ -15,11 +17,32 @@ def sign_in():
 
 @app.route('/main', methods=['GET', 'POST'])
 def transfer():
+    print('in main')
     name = request.form.get('NAME')
+    print(name)
     if type(name) == str:
         return redirect(url_for('chat', name=name))
 
-@app.route('/chat', methods=['GET', 'POST'])
+@app.route('/chat', methods=['GET','POST'])
 def chat():
+    global message
+    message = request.form.get("MESSAGE")
+    print(message, 'fw')
+    connection = pymysql.connect(host='localhost', user='root', password='root', db='pythondb', port=3306,
+                                 cursorclass=pymysql.cursors.DictCursor)
+    cursor_obj = connection.cursor()
+    cursor_obj.execute(f'INSERT INTO prabhatbot (messages)\nVALUES ({message})')
+    data = cursor_obj.fetchall
+    print(data)
+    cursor_obj.close()
+    connection.close()
+
     return render_template('index.html')
 
+
+#
+# @app.route("/savingMessages", methods=['GET', 'POST'])
+# def saveMessages():
+#
+#
+#
